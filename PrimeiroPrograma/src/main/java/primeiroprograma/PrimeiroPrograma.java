897571d2b2e037648cd05f0bab554997ca656e55
Willamy Ungary <willamy.ungary@gmail.com>
2026-09-20 23:16:09 -0300

    helloworld

---
+++ PrimeiroPrograma/src/main/java/primeiroprograma/PrimeiroPrograma.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package primeiroprograma;
+
+/**
+ *
+ * @author willamy
+ */
+public class PrimeiroPrograma {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
